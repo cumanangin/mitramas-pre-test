896b38e875394a85002d7eaf0fa4b441ee856d67
riioaditya <rioadityano@gmail.com>
2022-08-06 00:47:47 +0900

    style: responsive the sidebar

--- mitramas-pre-test/src/Components/Sidebars/index.jsx
+++ mitramas-pre-test/src/Components/Sidebars/index.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiHomeCircle, BiTask } from "react-icons/bi";
 import { HiOutlineTicket } from "react-icons/hi";
 import { TbBuildingSkyscraper } from "react-icons/tb";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCancelCircle } from "react-icons/im";
 import cocaCola from "../../assets/coca-cola-4.svg";
 
 const Sidebars = () => {
@@ -12,33 +14,57 @@ const Sidebars = () => {
     TbBuildingSkyscraper,
   ];
 
+  const [isResponsive, setIsReponsive] = useState(false);
+
+  const handleResponsive = () => {
+    setIsReponsive(true);
+    if (isResponsive === true) {
+      setIsReponsive(false);
+    }
+  };
+
   return (
     <>
-      <div className="w-[70px] h-screen bg-white">
-        <div className="flex justify-center items-center py-2">
-          {" "}
-          {/* logo */}
-          <div className="w-10">
-            <img src={cocaCola} />
-          </div>
-        </div>
+      <div className="mobile:flex mobile:justify-center mobile:items-center mobile:hover:cursor-pointer ">
+        {/* logo */}
         <div>
-          {/* div for icon sidebar */}
-          <ul className="flex-col">
-            {sidebarIcons.map((itemIcons, index) => {
-              const Icons = sidebarIcons[index];
-              return (
-                <li
-                  key={itemIcons.index}
-                  className="flex justify-center items-center py-3 hover:bg-[#F0F6F1] hover:cursor-pointer hover:duration-200"
-                >
-                  <div className="">
-                    <Icons size={20} />
-                  </div>
-                </li>
-              );
-            })}
-          </ul>
+          {isResponsive ? (
+            <div className="bg-white h-screen mobile:w-10 tablet:w-[60px]">
+              <div className="flex justify-center mobile:py-2">
+                <ImCancelCircle onClick={handleResponsive} />
+              </div>
+              <div className="flex justify-center items-center py-2">
+                {/* logo */}
+                <div className="mobile:w-10 pt-6">
+                  <img src={cocaCola} />
+                </div>
+              </div>
+              <div>
+                {/* div for icon sidebar */}
+                <ul className="flex-col">
+                  {sidebarIcons.map((itemIcons, index) => {
+                    const Icons = sidebarIcons[index];
+                    return (
+                      <li
+                        key={itemIcons.index}
+                        className="flex justify-center items-center py-3 hover:bg-[#F0F6F1] hover:cursor-pointer hover:duration-200"
+                      >
+                        <div className="mobile:w-7">
+                          <Icons size={25} />
+                        </div>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <div className="mobile:w-10 tablet:w-[60px]">
+              <div className="flex justify-center mobile:py-2">
+                <GiHamburgerMenu onClick={handleResponsive} />
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
