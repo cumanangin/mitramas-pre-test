89b37a1bca7092999a434814815ab3400073a062
riioaditya <rioadityano@gmail.com>
2022-08-06 13:29:30 +0900

    style: card for profile company

--- mitramas-pre-test/tailwind.config.cjs
+++ mitramas-pre-test/tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        logo: "1.95px 1.95px 2.6px rgba(0, 0, 0, 0.15)",
+      },
+    },
     screens: {
       mobile: "320px",
       tablet: "768px",
